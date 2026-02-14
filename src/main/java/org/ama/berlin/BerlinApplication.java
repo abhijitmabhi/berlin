a24cc4b91587b7c501a7db4e9a5e4c5debaba1fe
Abhijit Mondal Abhi <abhijit.abhi@n26.com>
2026-02-14 12:06:33 +0100

    Initialize Project

---
+++ src/main/java/org/ama/berlin/BerlinApplication.java
@@ -0,0 +1,13 @@
+package org.ama.berlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerlinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerlinApplication.class, args);
+    }
+
+}
