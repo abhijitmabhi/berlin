4b3b13130468ad27f03157c19df0954fa72c4476
Abhijit Mondal Abhi <abhijit.abhi@n26.com>
2026-02-19 09:51:01 +0100

    feat: - migration fix

--- src/test/resources/db/migration-h2/V2__seed_orders.sql
+++ src/test/resources/db/migration-h2/V2__seed_orders.sql
@@ -1,4 +1,3 @@
 insert into orders (id, title, description, order_date_time) values
 (1, 'First Order', 'Seed data order', now()),
-(2, 'Second Order', 'Another seed data order', now())
-on conflict (id) do nothing;
+(2, 'Second Order', 'Another seed data order', now());
