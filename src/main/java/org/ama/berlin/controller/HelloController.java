bea6277033a85691e2fdd4e1d345dfb3db1256e6
Abhijit Mondal Abhi <abhijit.abhi@n26.com>
2026-02-22 00:01:06 +0100

    fix: health endpoint

--- src/main/java/org/ama/berlin/controller/HelloController.java
+++ src/main/java/org/ama/berlin/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.ama.berlin.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello from Berlin service";
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("I am so healthy!");
     }
 }
