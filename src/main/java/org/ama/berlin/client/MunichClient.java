2f71f96123d3a52bdc086d120fa15b5fd66e9ab1
Abhijit Mondal Abhi <abhijit.abhi@n26.com>
2026-02-28 12:01:32 +0100

    - Circuitbreaker added

--- src/main/java/org/ama/berlin/client/MunichClient.java
+++ src/main/java/org/ama/berlin/client/MunichClient.java
@@ -1,11 +1,14 @@
 package org.ama.berlin.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.ama.berlin.dto.MunichCustomer;
+import org.ama.berlin.exception.MunichClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -19,6 +22,7 @@ public class MunichClient {
 
     private final WebClient munichWebClient;
 
+    @CircuitBreaker(name = "munich")
     @Retry(name = "munich")
     public List<MunichCustomer> getAllCustomers() {
         log.info("Calling Munich /api/v1/customers");
@@ -26,7 +30,18 @@ public class MunichClient {
         return munichWebClient.get()
                 .uri("/api/customer/getCustomers")
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<MunichCustomer>>() {})
+                .onStatus(HttpStatusCode::is4xxClientError, response ->
+                        response.bodyToMono(String.class)
+                                .defaultIfEmpty("")
+                                .map(body -> new MunichClientException("Munich returned 4xx: " + response.statusCode())))
+
+                .onStatus(HttpStatusCode::is5xxServerError, response ->
+                        response.bodyToMono(String.class)
+                                .defaultIfEmpty("")
+                                .map(body -> new MunichClientException("Munich returned 5xx: " + response.statusCode())))
+
+                .bodyToMono(new ParameterizedTypeReference<List<MunichCustomer>>() {
+                })
                 .timeout(Duration.ofSeconds(2))
                 .block();
     }
